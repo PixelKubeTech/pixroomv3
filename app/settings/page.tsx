b09dfbb05f7736b25f020ff37c0193e2131e822b
Dinesh <dineshkumar.indarmal@gmail.com>
2024-05-25 19:07:19 +0530

    build isssue fixed

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,56 +1,56 @@
-'use client'
-import React from 'react'
-import { useRouter } from 'next/navigation';
-import {CircularProgress} from '@mui/material'
-import RandomDigits from './RandomDigits';
-import {DeviceService} from '../../services'
-import { useSearchParams } from 'next/navigation'
-import { IErrorInfo,IResponse } from './SettingsInterface';
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { CircularProgress } from "@mui/material";
+import RandomDigits from "./RandomDigits";
+import { DeviceService } from "../../services";
+import { useSearchParams } from "next/navigation";
+import { IErrorInfo, IResponse } from "./SettingsInterface";
 
-function Settings(props: { macAddress: string }) {
+function Settings() {
   const router = useRouter();
-  const searchParams = useSearchParams()
-  const macAddress = searchParams.get('macaddress')
-  let [result,setResult] = React.useState(null)
-  let [loading,setLoading] = React.useState(true)
-  let [errorInfo,setErrorInfo] = React.useState<IErrorInfo>({
-    "code":"",
-    "message":""
-  })
+  const searchParams = useSearchParams();
+  const macAddress = searchParams.get("macaddress");
+  let [result, setResult] = React.useState(null);
+  let [loading, setLoading] = React.useState(true);
+  let [errorInfo, setErrorInfo] = React.useState<IErrorInfo>({
+    code: "",
+    message: "",
+  });
   React.useEffect(() => {
     // Function to fetch data and handle redirection
-   
 
     const fetchData = async () => {
       try {
         // Fetch data from the provided route
-        const serialResponse:IResponse = await DeviceService.getDeviceInfo({serialNumber:macAddress?macAddress:''});
-        console.log('serialResponse',serialResponse)
+        const serialResponse = await DeviceService.getDeviceInfo({
+          serialNumber: macAddress ? macAddress : "",
+        });
+        console.log("serialResponse", serialResponse);
         if (serialResponse.success) {
-          console.log('spaceId',serialResponse?.result.spaceId)
-          router.push(`/meeting?spaceId=${serialResponse.result.spaceId}`)
-          setLoading(false)
-        }else if(!serialResponse.success){
-          setLoading(false)
-          setErrorInfo((error)=>({
+          console.log("spaceId", serialResponse?.result.spaceId);
+          router.push(`/meeting?spaceId=${serialResponse.result.spaceId}`);
+          setLoading(false);
+        } else if (!serialResponse.success) {
+          setLoading(false);
+          setErrorInfo((error) => ({
             ...error,
-            code:"Space Id not assigned",
-            message:"No device assigned to serial number"
-          }))
-          
-        }else{
-          console.log('serialResponse',serialResponse)
-          setLoading(false)
-          setErrorInfo((error)=>({
+            code: "Space Id not assigned",
+            message: "No device assigned to serial number",
+          }));
+        } else {
+          console.log("serialResponse", serialResponse);
+          setLoading(false);
+          setErrorInfo((error) => ({
             ...error,
-            code:"Error",
-            message:"No device found"
-          }))
+            code: "Error",
+            message: "No device found",
+          }));
         }
       } catch (error) {
-        setLoading(false)
-        setResult(null)
-        console.error('Error fetching data:', error);
+        setLoading(false);
+        setResult(null);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -60,15 +60,13 @@ function Settings(props: { macAddress: string }) {
 
   return (
     <div className="h-[100vh] w-[100vw] flex flex-col items-center justify-center">
-      {
-        loading ? <CircularProgress/>:  <RandomDigits result={result} errorInfo={errorInfo}/>
-      }
-      
-    
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <RandomDigits result={result} errorInfo={errorInfo} />
+      )}
     </div>
-  )
+  );
 }
 
-
-
-export default Settings
+export default Settings;
