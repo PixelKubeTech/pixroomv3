6d5206bdb3baf9df32b54e6563cf219bb791c946
tech <tech@pixelkube.io>
2025-01-08 12:50:53 +0000

    Address the free slot issue and ensur upcoming meetings is used correctly

--- components/Player/MeetingCalenderContainer/BottomComponent/TimelineComponent/index.tsx
+++ components/Player/MeetingCalenderContainer/BottomComponent/TimelineComponent/index.tsx
@@ -4,19 +4,13 @@ import "./scrollbarthin.css";
 import { useAppStore } from "@/app/store/appStore";
 
 function TimelineComponent(props) {
-    const {
-      spaceInfo,
-      deviceInfo,
-      themeInfo,
-      events,
-      selectedDay,
-      upcomingEventsByDay,
-      loadFromLocalStorage
-    } = useAppStore();
+  const {
+    selectedDay,
+    upcomingEventsByDay,
+  } = useAppStore();
 
-    useEffect(() => {
-      loadFromLocalStorage();
-    }, [selectedDay]);
+  useEffect(() => {
+  }, [selectedDay, upcomingEventsByDay]);
 
   const [selectedSlots, setSelectedSlots] = useState<number[]>([]);
   const [selectedMeetingIndex, setSelectedMeetingIndex] = useState<number>(-1);
@@ -24,59 +18,50 @@ function TimelineComponent(props) {
     props.eventClick(data);
   };
   const { showFreeSlots } = props;
-  let meetingInfo: any = null;
+  let meetingInfo = upcomingEventsByDay[selectedDay] ?? [];
 
-  if (props.meetingInfo != null && props.meetingInfo.length > 0) meetingInfo = props.meetingInfo;
-  else if (
-    props.meetingInfo != null &&
-    props.meetingInfo.meetingInfo != null 
-    //&&
-    //props.meetingInfo.meetingInfo.length > 0
-  )
-    meetingInfo = props.meetingInfo.meetingInfo;
-    meetingInfo = meetingInfo?.sort((a, b) => a.bookingDetails.from.localeCompare(b.bookingDetails.from));
+  meetingInfo = meetingInfo?.sort((a, b) => a.bookingDetails.from.localeCompare(b.bookingDetails.from));
 
-    const findFreeSlots: any = (meetings) => {
-      const freeSlots: any = [];
-      let lastEndTime = "08:00";
-  
-      meetings.forEach((meeting) => {
-        if (lastEndTime < meeting.bookingDetails.from) {
-          freeSlots.push({
-            bookingDetails: {
-              from: lastEndTime,
-              to: meeting.bookingDetails.from,
-              meetingName: "Room Available",
-            },
-            isAvailable: true,
-          });
-        }
-        lastEndTime = meeting.bookingDetails.to;
-      });
-  
-      // Check for free time after the last meeting till 8 PM
-      if (lastEndTime < "20:00") {
+  const findFreeSlots: any = (meetings) => {
+    const freeSlots: any = [];
+    let lastEndTime = "08:00";
+
+    meetings.forEach((meeting) => {
+      if (lastEndTime < meeting.bookingDetails.from) {
         freeSlots.push({
           bookingDetails: {
             from: lastEndTime,
-            to: "20:00",
+            to: meeting.bookingDetails.from,
             meetingName: "Room Available",
           },
           isAvailable: true,
         });
       }
-  
-      return freeSlots;
-    };
-  
-    //const freeSlots = findFreeSlots(meetingInfo || []);
-    const freeSlots = showFreeSlots ? findFreeSlots(meetingInfo || []) : [];
-  
-    // Merge meetings and free slots, then sort
-    const combinedList = [...meetingInfo, ...freeSlots].sort((a, b) => a.bookingDetails.from.localeCompare(b.bookingDetails.from));
+      lastEndTime = meeting.bookingDetails.to;
+    });
+
+    // Check for free time after the last meeting till 8 PM
+    if (lastEndTime < "20:00") {
+      freeSlots.push({
+        bookingDetails: {
+          from: lastEndTime,
+          to: "20:00",
+          meetingName: "Room Available",
+        },
+        isAvailable: true,
+      });
+    }
+
+    return freeSlots;
+  };
+
+  const freeSlots = showFreeSlots ? findFreeSlots(meetingInfo || []) : [];
+
+  // Merge meetings and free slots, then sort
+  const combinedList = [...meetingInfo, ...freeSlots].sort((a, b) => a.bookingDetails.from.localeCompare(b.bookingDetails.from));
   return (
     <div className={`h-full overflow-scroll max-h-100 flex flex-col basis-1/2 gap-2 thin-scrollbar`}>
-      {upcomingEventsByDay[selectedDay]?.map((data, index) => (
+      {combinedList.map((data, index) => (
         <MeetingBlock
           key={index}
           isAvailable={data.isAvailable}
@@ -86,7 +71,7 @@ function TimelineComponent(props) {
           setStartTime={props.setStartTime}
           setEndTime={props.setEndTime}
           selectedSlots={selectedSlots}
-          setSelectedSlots={setSelectedSlots}s
+          setSelectedSlots={setSelectedSlots} s
           currentIndex={index}
           selectedMeetingIndex={selectedMeetingIndex}
           setSelectedMeetingIndex={setSelectedMeetingIndex}
